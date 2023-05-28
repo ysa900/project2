5b021fcf9228020b3df4b441448be56b089ec42e
ysa900 <ysa7176@naver.com>
2023-05-28 14:34:41 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,16 +1,13 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
 
-static #users = {
-        id: ["woorimIt", "나개발", "김팀장"],
-        psword: ["1234","1234","123456"],
-        names: ["우리밋", "나개발","김팀장"],
-    };
 
     static getUsers(...fields){
 
-        const users = this.#users;
+       // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field)=>{
             if(users.hasOwnProperty(field)){
                 newUsers[field]= users[field];
@@ -21,20 +18,31 @@ static #users = {
     }
 
     static getUserInfo(id){
-        const users = this.#users;
+  
+      return fs
+      .readFile("./src/databases/users.json")
+      .then((data)=> {
+        return this.#getUserInfo(data, id);
+            })
+       .catch(console.error);
+    }
+    
+    static #getUserInfo(data, id){
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users);
-        const userInfo = Object.keys(users).reduce((newUser,info)=>{
+        const userInfo = usersKeys.reduce((newUser,info)=>{
             newUser[info] = users[info][idx];
             return newUser;
-
         },{});
         return userInfo;
     }
-    
+
+
+
     static save(userInfo)   {
 
-        const users = this.#users;
+       // const users = this.#users;
         users.id.push(userInfo.id);
         users.names.push(userInfo.name);
         users.psword.push(userInfo.psword);
